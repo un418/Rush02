935e9525024e4468b74fa8b698988dab4c570681
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-15 17:04:04 +0000

    remove unused fonction

--- ex00/lib/ourheader.h
+++ ex00/lib/ourheader.h
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_is_number(char *str);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
