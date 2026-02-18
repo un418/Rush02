0e1937223f671a34dcee2ef99273c2f70748020c
Adam Fernandes <adaferna@student.42lisboa.com>
2026-02-18 14:59:02 +0000

    add comment for main

--- ex00/main.c
+++ ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 16:21:11 by adaferna          #+#    #+#             */
-/*   Updated: 2026/02/15 19:34:06 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/02/18 14:58:45 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,20 @@ int	main(int argc, char **argv)
 	char	*dict;
 	char	*filename;
 
+	// en: Need to init the varaible of type ptr for compilation
+	// pt: 
 	filename = malloc(sizeof(char *));
+	//en: Call the function to manage input from user: 1 arg use default arg ; 2arg use arg[1] as dict.file.path
+	// 		if the return of ft_arg() function end the program and return 1 to the system (means errors)
 	if (ft_arg(argc, argv, filename) != 0)
 		return (1);
+	// en: Allocate memory to the char that will store the dict content.
+	// 		-> ft_filesize return the size of the file
+	// 		-> +1 to ensure that we will be able to put the '\O' to symbolise the name of the string
 	dict = malloc(ft_filesize(filename)+ 1);
+	// en: Call the function that will copy the content of "filename" in the variable created just before called "dict"
 	ft_file_to_char(filename, dict);
+	// en: Call the function to read the dict, this function will call other function to parse the dictionary 
+	// 		and return the desrired value if needed.
 	return (ft_read_dict(dict, argv, argc, filename));
 }
